750efee9495119deba55abbc2ceab1225604a305
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-06-19 11:16:33 +0100

    Run dagster project scaffold

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ccfc_yt_dags",
+    packages=find_packages(exclude=["ccfc_yt_dags_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
