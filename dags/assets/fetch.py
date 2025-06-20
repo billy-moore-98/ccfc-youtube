d2754e70206318eb4617267ff98f841aa17d8a6d
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-06-20 14:27:10 +0100

    Add log statements

--- dags/assets/fetch.py
+++ dags/assets/fetch.py
@@ -36,6 +36,7 @@ def fetch_videos(
     """Fetches coventry city fc videos from the YouTube API with full pagination support"""
     # date partiton formatting
     date_partition = context.partition_key
+    context.log.info(f"Fetching videos for partition: {date_partition}")
     published_after = datetime.strptime(date_partition, "%Y-%m-%d").replace(tzinfo=timezone.utc)
     published_before = _get_end_of_month(published_after)
     # setting request params
@@ -47,8 +48,10 @@ def fetch_videos(
     # set s3 key prefix
     s3_key_prefix = f"videos/year={published_after.year}/month={published_after.month}"
     # load state file if exists, populate if it doesn't
+    context.log.info(f"Loading state now from : {s3_key_prefix}/state.json")
     state = _load_state_file(s3, "bmooreawsbucket", s3_key_prefix+"/state.json")
     if not state:
+        context.log.info("State file not found, initializing now")
         state = {
             "query": query,
             "partition": date_partition,
@@ -57,6 +60,7 @@ def fetch_videos(
             "nextPageToken": None
         }
     # begin fetch
+    context.log.info("Beginning video fetch")
     try:
         for page in yt._client.get_videos_search(
             query=query,
@@ -65,11 +69,13 @@ def fetch_videos(
             paginate=True,
             page_token=state.get("nextPageToken", None)
         ):
+            context.log.info(f"Processing page with {len(page.get('items', []))} items")
             items = page.get("items", [])
             # loop through video items and upload to s3
             for item in items:
                 video_id = item.get("id", {}).get("videoId")
                 s3_key = f"{s3_key_prefix}/video_id={video_id}.json"
+                context.log.info(f"Uploading video item with ID: {video_id} to S3 at {s3_key}")
                 s3._client.put_object(
                     Bucket="bmooreawsbucket",
                     Body=json.dumps(item),
@@ -79,6 +85,7 @@ def fetch_videos(
             # update state file with next page token
             next_page_token = page.get("nextPageToken")
             if next_page_token:
+                context.log.info(f"Next page required. Updating state with next page token: {next_page_token}")
                 state["nextPageToken"] = next_page_token
                 s3._client.put_object(
                     Bucket="bmooreawsbucket",
@@ -97,3 +104,7 @@ def fetch_videos(
     except Exception as e:
         context.log.error(f"An unexpected error occured: {e}")
         raise
+
+@dg.asset
+def fetch_comments():
+    pass
