ed0fa6a32fab5721fbca12a7e8af7435ae96e332
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-06-20 14:10:54 +0100

    Correct assets import in definitions

--- dags/definitions.py
+++ dags/definitions.py
@@ -3,12 +3,12 @@ import os
 from dagster import Definitions, load_assets_from_modules
 from dotenv import load_dotenv
 
-from dags import assets
+from dags.assets import fetch
 from dags.resources import s3Resource, YtResource
 
 load_dotenv()
 
-all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules([fetch])
 
 defs = Definitions(
     assets=all_assets,
