f36f03fb35d408187ed28d79cdc1e42fd7cdc256
billy-moore-98 <billy.moore98@hotmail.co.uk>
2025-06-30 12:27:02 +0100

    Handle comments disabled error

--- dags/assets/fetch/comments.py
+++ dags/assets/fetch/comments.py
@@ -1,7 +1,7 @@
 import dagster as dg
 import json
 
-from ccfc_yt.exceptions import QuotaExceededError
+from ccfc_yt.exceptions import CommentsDisabledError, QuotaExceededError
 from dags.assets.fetch.utils import append_comments_to_comments_file, get_videos_list, load_state_file
 from dags.resources import s3Resource, YtResource
 from datetime import datetime, timedelta
@@ -49,43 +49,47 @@ def fetch_comments(
             if state["complete"]:
                 context.log.info("Comments fetch already completed, skipping")
                 continue
-            for page in yt._client.get_comments_thread(
-                video_id=video,
-                max_results=20,
-                optional_params=optional_params,
-                paginate=True,
-                stream=True,
-                page_token=state.get("nextPageToken", None),
-                max_pages=(5-state["pagesFetched"]) # 20 results * 5 pages = max 100 comments per video
-            ):
-                if not page.get("items", []):
-                    context.log.info(f"No comments for video id: {video}, continuing")
-                    continue
-                context.log.info(f"Processing page with {len(page.get('items', []))} items for video id: {video}")
-                # process comment and append to comments.jsonl file at s3 key
-                context.log.info(f"Uploading {len(page['items'])} comments for video id: {video} to S3 at {s3_key_prefix}/comments.jsonl")
-                append_comments_to_comments_file(
-                    s3=s3,
-                    bucket="ccfcyoutube",
-                    key=f"{s3_key_prefix}/comments.jsonl",
-                    new_items=page.get("items", [])
-                )
-                # update state file with pages fetched
-                state["pagesFetched"] += 1
-                # update state file with next page token
-                next_page_token = page.get("nextPageToken", None)
-                state["nextPageToken"] = next_page_token
-                # update state file completion
-                if not next_page_token or state["pagesFetched"] >= 5:
-                    state["complete"] = True
-                # update state file
-                context.log.info(f"Updating state file for video id: {video} at {s3_key_prefix}/state.json")
-                s3._client.put_object(
-                    Bucket="ccfcyoutube",
-                    Key=f"{s3_key_prefix}/state.json",
-                    Body=json.dumps(state),
-                    ContentType="application/json"
-                )
+            try:
+                for page in yt._client.get_comments_thread(
+                    video_id=video,
+                    max_results=20,
+                    optional_params=optional_params,
+                    paginate=True,
+                    stream=True,
+                    page_token=state.get("nextPageToken", None),
+                    max_pages=(5-state["pagesFetched"]) # 20 results * 5 pages = max 100 comments per video
+                ):
+                    if not page.get("items", []):
+                        context.log.info(f"No comments for video id: {video}, continuing")
+                        continue
+                    context.log.info(f"Processing page with {len(page.get('items', []))} items for video id: {video}")
+                    # process comment and append to comments.jsonl file at s3 key
+                    context.log.info(f"Uploading {len(page['items'])} comments for video id: {video} to S3 at {s3_key_prefix}/comments.jsonl")
+                    append_comments_to_comments_file(
+                        s3=s3,
+                        bucket="ccfcyoutube",
+                        key=f"{s3_key_prefix}/comments.jsonl",
+                        new_items=page.get("items", [])
+                    )
+                    # update state file with pages fetched
+                    state["pagesFetched"] += 1
+                    # update state file with next page token
+                    next_page_token = page.get("nextPageToken", None)
+                    state["nextPageToken"] = next_page_token
+                    # update state file completion
+                    if not next_page_token or state["pagesFetched"] >= 5:
+                        state["complete"] = True
+                    # update state file
+                    context.log.info(f"Updating state file for video id: {video} at {s3_key_prefix}/state.json")
+                    s3._client.put_object(
+                        Bucket="ccfcyoutube",
+                        Key=f"{s3_key_prefix}/state.json",
+                        Body=json.dumps(state),
+                        ContentType="application/json"
+                    )
+            except CommentsDisabledError as e:
+                context.log.warning(f"Comments have been disabled for video with id {video}, skipping now")
+                continue
     except QuotaExceededError as e:
         # YouTube Data API quota resets at midnight PT
         # Schedule a retry for this time
